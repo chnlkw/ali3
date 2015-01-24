fcfff8537ee4e0c948e0f6369d0e5f26b5588784
chnlkw <chnlkw@gmail.com>
2015-01-24 17:14:46 +0800

    add myentry

---
+++ src/MyEntry.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+
+final class MyEntry<K, V> implements Map.Entry<K, V> {
+    private final K key;
+    private V value;
+
+    public MyEntry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public V setValue(V value) {
+        V old = this.value;
+        this.value = value;
+        return old;
+    }
+}
